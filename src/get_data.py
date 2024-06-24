2b7649b68b6d9b78a46513f57feb8f11a0c905c2
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-06-24 18:13:18 +0530

    get_data_added

---
+++ src/get_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+import yaml
+import argparse
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+        return config
+    
+def get_data(config_path):
+    config = read_params(config_path)
+    data_path = config["data_source"]["s3_source"]
+    df = pd.read_csv(data_path,sep=",")
+    print (df.head())
+    return df
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parseed_args = args.parse_args()
+    data = get_data(config_path=parseed_args.config)
+    
