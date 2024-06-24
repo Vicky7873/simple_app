0b4685a744595261c5de3aa397e3a1504b430f7b
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-06-24 19:17:35 +0530

    stage one

--- src/get_data.py
+++ src/get_data.py
@@ -16,6 +16,8 @@ def get_data(config_path):
     print (df.head())
     return df
 
+# extra code
+
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument('--config', default='params.yaml')
