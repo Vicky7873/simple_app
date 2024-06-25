6081a765d8d1d8c91ffdf73c7552c925689779ff
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-06-25 12:07:02 +0530

    setupdone

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="bhikipallai",
+    author_email="vicky.pallai900@gmail.com",
+    description="winesq",
+    packages=find_packages(),
+    license="MIT",
+)
